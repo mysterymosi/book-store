ebb424fa58f31e71caa12bbee14627df0a3a6c7d
mysterymosim <okimmosimiracle@gmail.com>
2020-09-24 14:37:52 +0000

    initial file structure setup

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="wrapper">
+      <TheHeader />
+      <TheSidebar />
+      <main role="main" class="main-content">
+        <router-view />
+      </main>
     </div>
-    <router-view />
-  </div>
 </template>
 
+<script>
+import TheHeader from "@/components/TheHeader.vue";
+import TheSidebar from "@/components/TheSidebar.vue";
+export default {
+  components: {
+    TheHeader,
+    TheSidebar
+  }
+}
+</script>
+
 <style>
+@import "./assets/css/simplebar.css";
+@import "./assets/css/feather.css";
+@import "./assets/css/select2.css";
+@import "./assets/css/dropzone.css";
+@import "./assets/css/uppy.min.css";
+@import "./assets/css/jquery.steps.css";
+@import "./assets/css/jquery.timepicker.css";
+@import "./assets/css/quill.snow.css";
+@import "./assets/css/daterangepicker.css";
+@import "./assets/css/app-light.css";
+@import "./assets/css/app-dark.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
